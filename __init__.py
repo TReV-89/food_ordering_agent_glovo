b872cc5f10394073bab4019a53ce2b6859c6bb2d
[Trevor Saaka] <[trevie89@gmail.com]>
2025-08-18 20:32:09 +0300

    initialized project structure

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+
+load_dotenv()
+api_key = os.getenv("api_key")
+model = os.getenv("model")
+
+llm = ChatGoogleGenerativeAI(
+    model=model, google_api_key=api_key, temperature=0.5, max_tokens=None
+)
